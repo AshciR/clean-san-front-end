d2df62efeeee2620a677ced8a50b4402bf9444a7
Richard Walker <richard.walker.90@gmail.com>
2022-09-29 19:29:44 -0400

    CWA-140: Part 3: Added AddContractForm functionality (#36)
    What:
    Added API service for POST contracts
    Added handlers for the Mock service worker
    
    Why:
    Part 3 of addding the ability to add contracts to the system

--- src/mocks/clientsHandlers.ts
+++ src/mocks/clientsHandlers.ts
@@ -1,6 +1,13 @@
 import {rest} from "msw";
 import {getClientsResponse} from "./clientsEndpointResponses";
-import {AddClientRequest, AddClientResponse, GetClientsResponse} from "../services/clients.services";
+import {
+  AddClientRequest,
+  AddClientResponse,
+  AddContractRequest,
+  GetClientsResponse
+} from "../services/clients.services";
+import {ContractResponse} from "../services/shared-responses";
+import {ContractStatus} from "../shared/Contract.model";
 
 
 const getClientsHandler = rest.get('*/v1/clients', (req, res, context) => {
@@ -32,6 +39,65 @@ const addClientHandler = rest.post('*/v1/clients', (req, res, context) => {
 
 });
 
+const addContractHandler = rest.post('*/v1/clients/:clientId/contracts', (req, res, context) => {
+
+  const {clientId} = req.params;
+  const clientWithContract = getClientsResponse.clients.find(client => client.id === Number(clientId));
+
+  if (clientWithContract) {
+
+    const addContractRequest = req.body as AddContractRequest;
+
+    const response: ContractResponse = {
+      id: determineNextContractId(getClientsResponse),
+      clientId: Number(clientId),
+      startDate: addContractRequest.startDate,
+      endDate: addContractRequest.endDate,
+      serviceFrequency: addContractRequest.serviceFrequency,
+      status: ContractStatus.INACTIVE,
+    };
+
+    return res(
+      context.status(201),
+      context.json(response)
+    );
+
+  }
+
+  // Client doesn't exist, so we can't create a contract
+  return res(
+    context.status(404)
+  );
+
+});
+
+const determineNextContractId = (getClientsResponse: GetClientsResponse) => {
+
+  const allContracts = getClientsResponse.clients
+    .map(client => client.contracts);
+
+  // The mapping returns an array of array [ [{}, {}], [{}] ]
+  // We need to convert it to [{}, {}, {}]
+  const flattenedContracts = flattenArray(allContracts);
+  flattenedContracts.sort((a: ContractResponse, b: ContractResponse) => a.id - b.id);
+
+  return flattenedContracts[flattenedContracts.length - 1].id + 1;
+}
+
+// @ts-ignore
+const flattenArray = (data: any[]) => {
+
+  // our initial value this time is a blank array
+  // call reduce on our data
+  return data.reduce((total, value) => {
+    // if the value is an array then recursively call reduce
+    // if the value is not an array then just concat our value
+
+    // eslint-disable-next-line @typescript-eslint/no-unused-vars
+    return total.concat(Array.isArray(value) ? flattenArray(value) : value);
+  }, []);
+};
+
 export const clientsHandlers = [
-  getClientsHandler, addClientHandler
+  getClientsHandler, addClientHandler, addContractHandler
 ]
